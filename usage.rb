8c63802e518768ed0bb10c86df15c914a7d8922f
Hector Castro <hectcastro@gmail.com>
2010-03-03 16:11:20 -0500

    Added /editor route that returns editors and a count of their line changes.

--- usage.rb
+++ usage.rb
@@ -18,12 +18,34 @@ get '/' do
   start_time = Time.at(params[:start].to_i)
   end_time = Time.at(params[:end].to_i)
   difference = (end_time - start_time).to_i
-  
+
   if params[:start].nil? or params[:end].nil? or difference <= 0
     throw :halt, [400, {:message => "Bad request"}.to_json]
   elsif difference > 864000
     throw :halt, [413, {:message => "Request Entity Too Large"}.to_json]
   else
     {:changes => Change.sum(:line_changes, :changed_at => (Time.at(params[:start].to_i)..Time.at(params[:end].to_i))) }.to_json
+  end
+end
+
+get '/user/?' do
+  start_time = Time.at(params[:start].to_i)
+  end_time = Time.at(params[:end].to_i)
+  difference = (end_time - start_time).to_i
+
+  if params[:start].nil? or params[:end].nil? or difference <= 0
+    throw :halt, [400, {:message => "Bad request"}.to_json]
+  elsif difference > 864000
+    throw :halt, [413, {:message => "Request Entity Too Large"}.to_json]
+  else
+    results = repository(:default).adapter.select(
+      'SELECT DISTINCT(editor), SUM(line_changes) FROM changes GROUP BY editor ORDER BY editor'
+    )
+
+    output = [ ]
+    results.each do |result|
+      output << "#{{ result["editor"] => result["sum(line_changes)"] }}"
+    end
+    output.to_json
   end
 end
