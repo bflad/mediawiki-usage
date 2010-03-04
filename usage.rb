80d14d4e8f58bee75405d67295c2d3005b6e6240
Hector Castro <hectcastro@gmail.com>
2010-03-03 20:11:05 -0500

    Changed paths so that they make more sense + removed date range constraints from editors/ and pages/.

--- usage.rb
+++ usage.rb
@@ -14,21 +14,13 @@ before do
   headers "Content-Type" => "application/json; charset=utf-8"
 end
 
-get '/' do
-  start_time = Time.at(params[:start].to_i)
-  end_time = Time.at(params[:end].to_i)
-  difference = (end_time - start_time).to_i
-
-  if params[:start].nil? or params[:end].nil? or difference <= 0
-    throw :halt, [400, {:message => "Bad request"}.to_json]
-  elsif difference > 864000
-    throw :halt, [413, {:message => "Request Entity Too Large"}.to_json]
-  else
-    {:changes => Change.sum(:line_changes, :changed_at => (Time.at(params[:start].to_i)..Time.at(params[:end].to_i))) }.to_json
+helpers do
+  def structs_to_hashes(structs, k1, k2)
+    structs.inject([ ]) { |output, struct| output << { struct[k1] => struct[k2] } }
   end
 end
 
-get '/editor/?' do
+get '/count/?' do
   start_time = Time.at(params[:start].to_i)
   end_time = Time.at(params[:end].to_i)
   difference = (end_time - start_time).to_i
@@ -38,36 +30,20 @@ get '/editor/?' do
   elsif difference > 864000
     throw :halt, [413, {:message => "Request Entity Too Large"}.to_json]
   else
-    results = repository(:default).adapter.select(
-      'SELECT DISTINCT(editor), SUM(line_changes) FROM changes GROUP BY editor ORDER BY editor'
-    )
-
-    output = [ ]
-    results.each do |result|
-      output << { result["editor"] => result["sum(line_changes)"] }
-    end
-    output.to_json
+    {:changes => Change.sum(:line_changes, :changed_at => (start_time..end_time))}.to_json
   end
 end
 
-get '/topic/?' do
-  start_time = Time.at(params[:start].to_i)
-  end_time = Time.at(params[:end].to_i)
-  difference = (end_time - start_time).to_i
+get '/editors/?' do
+  results = repository(:default).adapter.select(
+    'SELECT DISTINCT(editor), SUM(line_changes) AS totals FROM changes GROUP BY editor ORDER BY editor'
+  )
+  structs_to_hashes(results, "editor", "totals").to_json
+end
 
-  if params[:start].nil? or params[:end].nil? or difference <= 0
-    throw :halt, [400, {:message => "Bad request"}.to_json]
-  elsif difference > 864000
-    throw :halt, [413, {:message => "Request Entity Too Large"}.to_json]
-  else
-    results = repository(:default).adapter.select(
-      'SELECT DISTINCT(topic), SUM(line_changes) FROM changes GROUP BY topic ORDER BY topic'
-    )
-
-    output = [ ]
-    results.each do |result|
-      output << { result["topic"] => result["sum(line_changes)"] }
-    end
-    output.to_json
-  end
+get '/pages/?' do
+  results = repository(:default).adapter.select(
+    'SELECT DISTINCT(topic), SUM(line_changes) as totals FROM changes GROUP BY topic ORDER BY topic'
+  )
+  structs_to_hashes(results, "topic", "totals").to_json
 end
