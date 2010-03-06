6c46582e888f173552253f78ce74c35e99b3dd28
Hector Castro <hectcastro@gmail.com>
2010-03-05 23:03:36 -0500

    Cleaned up the methods + added start/end range for users/ pages/.

--- usage.rb
+++ usage.rb
@@ -1,50 +1,80 @@
 require 'rubygems'
 require 'sinatra'
-require 'mysql'
 require 'dm-core'
 require 'dm-aggregates'
 require 'json'
 require 'lib/change'
 
 configure :production, :development do
-  CONFIG = YAML.load_file("config/database.yml") if File.exists?("config/database.yml") and File.readable?("config/database.yml")
-  DataMapper.setup(:default, "mysql://#{CONFIG['username']}:#{CONFIG['password']}@#{CONFIG['host']}/#{CONFIG['database']}")
+  TEN_DAYS = 864000
+  @config = YAML.load_file("config/database.yml") if File.exists?("config/database.yml")
+  DataMapper.setup(:default, {
+    :adapter => @config['adapter'],
+    :host => @config['host'],
+    :username => @config['username'],
+    :password => @config['password'],
+    :database => @config['database']
+  })
 end
 
 before do
   headers "Content-Type" => "application/json; charset=utf-8"
 end
 
-helpers do
-  def structs_to_hashes(structs, k1, k2)
-    structs.inject([ ]) { |output, struct| output << { struct[k1] => struct[k2] } }
-  end
-end
-
 get '/count/?' do
-  start_time = Time.at(params[:start].to_i)
-  end_time = Time.at(params[:end].to_i)
-  difference = (end_time - start_time).to_i
-
-  if params[:start].nil? or params[:end].nil? or difference <= 0
+  if params[:start].nil? or params[:end].nil?
     throw :halt, [400, {:message => "Bad request"}.to_json]
-  elsif difference > 864000
-    throw :halt, [413, {:message => "Request Entity Too Large"}.to_json]
   else
-    {:changes => Change.sum(:line_changes, :changed_at => (start_time..end_time))}.to_json
+    start_time = Time.at(params[:start].to_i)
+    end_time = Time.at(params[:end].to_i)
+    difference = (end_time - start_time).to_i
+    
+    unless difference > 0 and difference <= TEN_DAYS
+      throw :halt, [413, {:message => "Request Entity Too Large"}.to_json]
+    else
+      {:changes => Change.sum(:line_changes, :changed_at => (start_time..end_time))}.to_json
+    end
   end
 end
 
 get '/editors/?' do
-  results = repository(:default).adapter.select(
-    'SELECT DISTINCT(editor), SUM(line_changes) AS totals FROM changes GROUP BY editor ORDER BY editor'
-  )
-  structs_to_hashes(results, "editor", "totals").to_json
+  if params[:start].nil? or params[:end].nil?
+    throw :halt, [400, {:message => "Bad request"}.to_json]
+  else
+    start_time = Time.at(params[:start].to_i)
+    end_time = Time.at(params[:end].to_i)
+    difference = (end_time - start_time).to_i
+    
+    unless difference > 0 and difference <= TEN_DAYS
+      throw :halt, [413, {:message => "Request Entity Too Large"}.to_json]
+    else
+      results = repository(:default).adapter.select(
+        'SELECT DISTINCT(editor), SUM(line_changes) AS total FROM changes WHERE changed_at >= FROM_UNIXTIME(?) AND changed_at <= FROM_UNIXTIME(?) GROUP BY editor',
+        start_time.to_i,
+        end_time.to_i
+      )
+      results.inject([ ]) { |output, struct| output << { struct['editor'] => struct['total'].to_i } }.to_json
+    end
+  end
 end
 
 get '/pages/?' do
-  results = repository(:default).adapter.select(
-    'SELECT DISTINCT(topic), SUM(line_changes) as totals FROM changes GROUP BY topic ORDER BY topic'
-  )
-  structs_to_hashes(results, "topic", "totals").to_json
+  if params[:start].nil? or params[:end].nil?
+    throw :halt, [400, {:message => "Bad request"}.to_json]
+  else
+    start_time = Time.at(params[:start].to_i)
+    end_time = Time.at(params[:end].to_i)
+    difference = (end_time - start_time).to_i
+    
+    unless difference > 0 and difference <= TEN_DAYS
+      throw :halt, [413, {:message => "Request Entity Too Large"}.to_json]
+    else
+      results = repository(:default).adapter.select(
+        'SELECT DISTINCT(topic), SUM(line_changes) AS total FROM changes WHERE changed_at >= FROM_UNIXTIME(?) AND changed_at <= FROM_UNIXTIME(?) GROUP BY topic',
+        start_time.to_i,
+        end_time.to_i
+      )
+      results.inject([ ]) { |output, struct| output << { struct['topic'] => struct['total'].to_i } }.to_json
+    end
+  end
 end
